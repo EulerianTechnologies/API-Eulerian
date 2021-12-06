76e3626f6cf26614b1fff1162fdf57c9ea561bb1
Xavier <x.thorillon@eulerian.com>
2021-12-06 10:26:31 +0100

    + Add print hooks class

---
+++ lib/Eulerian/Edw/Hooks/Print.pm
@@ -0,0 +1,170 @@
+#/usr/bin/env perl
+###############################################################################
+#
+# @file Print.pm
+#
+# @brief Eulerian Data Warehouse Peer Print Hooks class Module definition.
+#
+# This module is aimed to print Eulerian Data Warehouse Analytics Results into
+# the terminal.
+#
+# @author Thorillon Xavier:x.thorillon@eulerian.com
+#
+# @date 26/11/2021
+#
+# @version 1.0
+#
+###############################################################################
+#
+# Setup module name
+#
+package Eulerian::Edw::Hooks::Print;
+#
+# Enforce compilor rules
+#
+use strict; use warnings;
+#
+# Inherited interface from Eulerian::Edw::Hooks
+#
+use parent 'Eulerian::Edw::Hooks';
+#
+# @brief Allocate a new Eulerian Data Warehouse Peer Hooks.
+#
+# @param $class - Eulerian Data Warehouse Peer Hooks Class.
+# @param $setup - Setup attributes.
+#
+# @return Eulerian Data Warehouse Peer Hooks instance.
+#
+sub new
+{
+  my ( $class, $setup ) = @_;
+  return $class->SUPER::new( $setup );
+}
+#
+# @brief Setup Eulerian Data Warehouse Peer Hooks.
+#
+# @param $self - Eulerian Data Warehouse Peer Hooks.
+# @param $setup - Setup entries.
+#
+sub setup
+{
+  my ( $self, $setup ) = @_;
+}
+#
+# @brief
+#
+# @param $self - Eulerian Data Warehouse Peer Hooks.
+# @param $uuid - UUID of Eulerian Analytics Analysis.
+# @param $start - Timerange begin.
+# @param $end - Timerange end.
+# @param $columns - Array of Columns definitions.
+#
+sub on_headers
+{
+  my ( $self, $uuid, $start, $end, $columns ) = @_;
+
+
+  print( "$self | $uuid | $start | $end\n" );
+
+
+  my $string = <<string_end;
+  {
+    . UUID      : $uuid
+    . TIMERANGE : {
+        begin : $start,
+        end : $end,
+      },
+    . HEADERS   : {
+string_end
+  foreach my $column ( @$columns ) {
+    if( ref( $column ) eq 'ARRAY' ) {
+      $string .= "        $column->[ 0 ] : $column->[ 1 ],\n";
+    } else {
+      $string .= "        $column : UNKNOWN,\n";
+    }
+  }
+  $string .= "      },\n  }\n";
+  print( $string );
+
+}
+#
+# @brief
+#
+# @param $self - Eulerian Data Warehouse Peer Hooks.
+# @param $uuid - UUID of Eulerian Analytics Analysis.
+# @param $rows - Array of Array of Columns values.
+#
+sub on_add
+{
+  my ( $self, $uuid, $rows ) = @_;
+  my $string = '';
+
+  for my $row ( @$rows ) {
+    for my $col ( @$row ) {
+      $string .= "$col | ";
+    }
+    $string .= "\n";
+  }
+  print( $string );
+
+}
+#
+# @brief
+#
+# @param $self - Eulerian Data Warehouse Peer Hooks.
+# @param $uuid - UUID of Eulerian Analytics Analysis.
+# @param $rows - Array of Array of Columns values.
+#
+sub on_replace
+{
+  my ( $self, $uuid, $rows ) = @_;
+  my $string = '';
+
+  for my $row ( @$rows ) {
+    for my $col ( @$row ) {
+      $string .= "$col | ";
+    }
+    $string .= "\n";
+  }
+  print( $string );
+
+}
+#
+# @brief
+#
+# @param $self - Eulerian Data Warehouse Peer Hooks.
+# @param $uuid - UUID of Eulerian Analytics Analysis.
+# @param $progress - Progression value.
+#
+sub on_progress
+{
+  my ( $self, $uuid, $progress ) = @_;
+  print( "PROGRESSION : $progress\n" );
+}
+#
+# @brief
+#
+# @param $self - Eulerian Data Warehouse Peer Hooks.
+# @param $uuid - UUID of Eulerian Analytics Analysis.
+# @param $token - AES Token or Bearer.
+# @param $errnum - Error number.
+# @param $err - Error description.
+# @param $updated - Count of updates on server.
+#
+sub on_status
+{
+  my ( $self, $uuid, $token, $errnum, $err, $updated ) = @_;
+  my $string = <<string_end;
+    $uuid : {
+     token => '$token',
+     error_code => $errnum,
+     error_msg => $err,
+    }
+string_end
+  print( $string );
+
+}
+#
+# Endup module properly
+#
+1;
