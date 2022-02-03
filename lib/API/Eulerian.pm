c35fdf84df9b130deaa6ec42d8132533b67a94cd
matjmat <mathieu@eulerian.com>
2022-02-03 15:07:42 +0100

    doc

--- lib/API/Eulerian.pm
+++ lib/API/Eulerian.pm
@@ -9,6 +9,22 @@ __END__
 
 =pod
 
-=head1 API::Eulerian
+=head1 NAME
+
+ API::Eulerian - Collection of modules for interacting with the Eulerian API
+
+=head1 SYNOPSIS
+
+ Check sub-modules depending on the use-case :
+ - API::Eulerian::EDW : for interacting with Eulerian DataWareHouse
+
+=head1 DESCRIPTION
+
+ This module is only a placeholder, check sub-modules for each different available API.
+
+=head1 AUTHOR
+
+ Mathieu Jondet <mathieu@eulerian.com>
+ Xavier Thorillon <xavier@eulerian.com>
 
 =cut
--- lib/API/Eulerian/EDW.pm
+++ lib/API/Eulerian/EDW.pm
@@ -41,3 +41,61 @@ sub get_csv_file {
 
 1;
 __END__
+
+=pod
+
+=head1 NAME
+
+ API::Eulerian::EDW - Simple interface for interacting with Eulerian DataWareHouse
+
+=head1 SYNOPSIS
+
+ use API::Eulerian::EDW();
+
+ my $edw = new API::Eulerian::EDW();
+ my $rh_query = $edw->get_csv_file( \%h_conf, $query );
+
+ if ( $rh_query->{error} ) {
+   print STDERR "Error: ".$rh_query->{error_msg}."\n";
+   exit(1);
+ }
+
+ my $path2csvfile = $rh_query->{path2file};
+
+ # Content is in the CSV file
+
+=head1 DESCRIPTION
+
+ This module is a simple wrapper for querying the EDW and get the CSV file containing the content.
+
+=head1 METHODS
+
+=over 8
+
+=item get_csv_file ( \%h_conf, $query )
+
+ %h_conf is a hash reference containing :
+
+=over 4
+
+=item grid : name of the grid
+
+=item token : generic authorization API token
+
+=item site : name of the site
+
+=back
+
+ $query : is a EDW valid query
+
+ The method returns a hash reference with containing error property set to 1 with error_msg, otherwise
+ the csv path is provided in path2file.
+
+=back
+
+=head1 AUTHOR
+
+ Mathieu Jondet <mathieu@eulerian.com>
+ Xavier Thorillon <xavier@eulerian.com>
+
+=cut
