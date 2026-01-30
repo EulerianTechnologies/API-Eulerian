c800e0f807a66a811c5fca5b6f4a74050b1cd6c9
THORCOMP <THORCOMP@Yahoo.fr>
2026-01-30 10:07:37 +0100

    + Add UseCases/RollMap/Sql.pm + Add UseCases/SanKeyMap/Sql.pm

---
+++ lib/API/Eulerian/EDW/UseCases/SanKeyMap/Sql.pm
@@ -0,0 +1,185 @@
+
+package API::Eulerian::EDW::UseCases::SanKeyMap;
+
+use API::Eulerian::EDW::Sql::Builder;
+use strict; use warnings;
+use base 'Exporter';
+
+our @EXPORT = qw( Sql );
+
+sub AddTimerange
+{
+  my ( $builder, $setup ) = @_;
+  $builder->timerange(
+    $setup->{ from }, $setup->{ to }
+    );
+}
+
+sub AddPageview
+{
+  my ( $builder, $setup ) = @_;
+  my @devices = @{ $setup->{ 'mdevicetypefull-name' } || [] };
+  my @subkeys = @{ $setup->{ 'subkey2-name' } || [] };
+  my $ndevices = scalar( @devices );
+  my $nsubkeys = scalar( @subkeys );
+  my $filter = '';
+
+  # Handle pageview filtering on devices if any
+  if( $ndevices == 1 ) {
+    $filter .= 'pageview.device.devicetype.type == ';
+    $filter .= "'";
+    $filter .= $devices[ 0 ];
+    $filter .= "'";
+  } elsif( $ndevices > 1 ) {
+    $filter .= 'IN( pageview.device.devicetype.type, ';
+    $filter .= join( ', ', map { '"' . $_ . '"' } @devices );
+    $filter .= ' )';
+  }
+
+  # Handle pageview filtering on subkey2 if any
+  if( $nsubkeys == 1 ) {
+    if( $ndevices ) { $filter .= " && "; }
+    $filter .= 'pageview.subkey2.name == ';
+    $filter .= "'";
+    $filter .= $subkeys[ 0 ];
+    $filter .= "'";
+  } elsif( $nsubkeys > 1 ) {
+    if( $ndevices ) { $filter .= " && "; }
+    $filter .= 'IN( pageview.subkey2.name, ';
+    $filter .= join( ', ', map { '"' . $_ . '"' } @subkeys );
+    $filter .= ' )';
+  }
+
+  # Add pageview reader
+  $builder->readers(
+    'pageview', 'ea:pageview', $setup->{ site }, $filter
+  );
+
+}
+
+sub AddClickview
+{
+  my ( $builder, $setup ) = @_;
+  my $media = $setup->{ 'media-shortname' } || undef;
+  my $imedia = int( $setup->{ 'media-id' } || 0 );
+
+  # Handle clickview if need it */
+  if( $imedia ) {
+    # Setup clickview filter
+    my $filter  = "clickview.channel.odmedia == '";
+    $filter .= $media;
+    $filter .= "'";
+    # Add clickview reader
+    $builder->readers(
+      'clickview', 'ea:clickview', $setup->{ site }, $filter
+    );
+  }
+
+}
+
+sub AddVisit
+{
+  my ( $builder, $setup ) = @_;
+  my $filter;
+
+  # Setup visit filter
+  $filter  = 'visit.last.pageview.timestamp + MINS( ';
+  $filter .= int( $setup->{ session } || 30 );
+  $filter .= ' ) < pageview.timestamp';
+
+  # Create visit
+  $builder->groups( 'visit', 'pageview', $filter );
+
+}
+
+sub AddJoin
+{
+  my ( $builder, $setup ) = @_;
+  my $media = $setup->{ 'media-shortname' } || undef;
+  my $imedia = int( $setup->{ 'media-id' } || 0 );
+
+  if( $imedia ) {
+    # Setup join filter
+    my $filter  = "clickview.timestamp > visit.first.pageview.timestamp && ";
+    $filter .= "clickview.timestamp <= visit.last.pageview.timestamp";
+    # Add join on clickview
+    $builder->joins( 'join', 'visit', 'clickview', $filter );
+  }
+
+  return $imedia;
+}
+
+sub AddOutputs
+{
+  my ( $builder, $prefix, $setup ) = @_;
+
+  # Add outputs
+  for my $ioutput ( 0 ... $setup->{ npages } - 1 ) {
+    my $output = $prefix;
+    $output .= "visit.items( $ioutput ).pageview.page.name";
+    $builder->outputs( $output );
+  }
+
+}
+
+sub AddFilter
+{
+  my ( $builder, $prefix, $setup ) = @_;
+  my $roots = $setup->{ 'roots' };
+
+  # Add outputs filter
+  if( defined( $roots ) && scalar( @$roots ) > 0 ) {
+    my $filter = '';
+
+    if( $prefix ne '' ) {
+      $filter .= $prefix;
+      $filter .= "visit.first.pageview.timestamp != NULL && ";
+    }
+    if( scalar( @$roots ) == 1 ) {
+      $filter .= $prefix;
+      $filter .= "visit.items( 0 ).pageview.page.name == '";
+      $filter .= $roots->[ 0 ];
+      $filter .= "'";
+    } elsif( scalar( @$roots ) > 1 ) {
+      $filter .= "IN( ";
+      $filter .= $prefix;
+      $filter .= "visit.items( 0 ).pageview.page.name, ";
+      $filter .= join( ', ', map { '"' . $_ . '"' } @$roots );
+      $filter .= " )";
+    }
+    $builder->filter( $filter );
+
+  }
+
+}
+
+sub AddThen
+{
+  my ( $builder, $setup ) = @_;
+  my $palmares = int( $setup->{ npalmares } || 5 );
+  $builder->thens( "SanKeyMap( $palmares )" );
+}
+
+sub Sql
+{
+  my $builder = new API::Eulerian::EDW::Sql::Builder();
+  my $master = 'visit';
+  my ( $setup ) = @_;
+  my $prefix = '';
+
+  $builder->mode( 'ROW' );
+  $builder->master( 'visit' );
+  AddTimerange( $builder, $setup );
+  AddPageview( $builder, $setup );
+  AddClickview( $builder, $setup );
+  AddVisit( $builder, $setup );
+  if( AddJoin( $builder, $setup ) ) {
+    $builder->master( 'join' );
+    $prefix = 'join.';
+  }
+  AddOutputs( $builder, $prefix, $setup );
+  AddFilter( $builder, $prefix, $setup );
+  AddThen( $builder, $setup );
+
+  return $builder->str();
+}
